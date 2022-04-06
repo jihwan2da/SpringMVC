b21dd953ba526f488bdaeca53b68b8574a83332b
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-04-06 17:36:12 +0900

    setting

---
+++ src/test/java/jihwan/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package jihwan.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
