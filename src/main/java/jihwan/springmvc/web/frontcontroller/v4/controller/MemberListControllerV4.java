eb8751f3ed208bb48504c8e4bd3a9303b580731a
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-05-08 23:52:16 +0900

    [pratice]: frontController v4 구현하기

---
+++ src/main/java/jihwan/springmvc/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,21 @@
+package jihwan.springmvc.web.frontcontroller.v4.controller;
+
+
+import jihwan.springmvc.domain.member.Member;
+import jihwan.springmvc.domain.member.MemberRepository;
+import jihwan.springmvc.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        List<Member> members = memberRepository.findAll();
+        model.put("members", members);
+        return "members";
+    }
+}
