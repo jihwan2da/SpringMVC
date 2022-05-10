b2a34dc695cd677ae0bb46e4eb1b2c5af584aca7
jihwan2da <88091743+jihwan2da@users.noreply.github.com>
2022-05-10 23:23:58 +0900

    [pratice]: frontController v5 구현하기(adapter)

---
+++ src/main/java/jihwan/springmvc/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,16 @@
+package jihwan.springmvc.web.frontcontroller.v5;
+
+
+import jihwan.springmvc.web.frontcontroller.ModelView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+
+    boolean supports(Object handler);
+
+    ModelView handler(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+}
